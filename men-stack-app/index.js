3d6ec79ef15a673b627c7841c419ac1b834c26ef
vladislav <bincha.1997@gmail.com>
2020-02-29 23:48:43 +0900

    added men-stack app

---
+++ men-stack-app/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const todoRouts = require("./routs/todos");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs"
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(todoRouts);
+
+async function start() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://vladislav:Parolyanet12@cluster0-2lbqo.mongodb.net/todos",
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      }
+    );
+    app.listen(PORT, () => {
+      console.log("Server has been started..");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
