a6e5d0793e493647d2ee80620a39a1e0b93e2f03
vladislav <bincha.1997@gmail.com>
2020-03-03 23:46:14 +0900

    feat: update routes

--- mern-stack-app/routes/auth.routes.js
+++ mern-stack-app/routes/auth.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = Router();
@@ -74,7 +76,8 @@ router.post(
         return res.status(400).json({ message: 'Неверный пароль, попробуйте снова' });
       }
 
-      
+      const token = jwt.sign({ userId: user.id }, config.get('jwtSecret'), { expiresIn: '1h' });
+      res.json({ token, userId: user.id });
     } catch (e) {
       res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова...' });
     }
