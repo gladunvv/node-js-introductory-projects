e11ef50cca113f276bfcf43bf64a377c2574e36a
vladislav <bincha.1997@gmail.com>
2020-03-04 21:16:08 +0900

    feat: update client

---
+++ mern-stack-app/client/src/pages/AuthPage.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import { useHttp } from '../hooks/http.hook';
+import { useMessage } from '../hooks/message.hook';
+
+export const AuthPage = () => {
+  const message = useMessage();
+  const { loading, error, request, clearError } = useHttp();
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  useEffect(() => {
+    message(error);
+    clearError();
+  }, [error, message, clearError]);
+
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const registerHendler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', { ...form });
+      message(data.message)
+    } catch {}
+  };
+
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h2>Сократи Сcылку</h2>
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Авторизация</span>
+            <div>
+              <div className="input-field">
+                <input
+                  className="yellow-input"
+                  placeholder="Введите email"
+                  id="email"
+                  type="text"
+                  name="email"
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="input-field">
+                <input
+                  className="yellow-input"
+                  placeholder="Введите пароль"
+                  id="password"
+                  type="password"
+                  name="password"
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+            </div>
+          </div>
+          <div className="card-action">
+            <button
+              className="btn yellow darken-4"
+              onClick={registerHendler}
+              disabled={loading}
+              style={{ marginRight: 20 }}
+            >
+              Войти
+            </button>
+            <button
+              className="btn grey lighten-1 black-text"
+              onClick={registerHendler}
+              disabled={loading}
+            >
+              Регистрация
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
