4f2171eef46b3a283f756f7a68a02a1b71ea5ed0
vladislav <bincha.1997@gmail.com>
2020-03-07 18:01:18 +0900

    feat: added detail page

--- mern-stack-app/client/src/App.jsx
+++ mern-stack-app/client/src/App.jsx
@@ -4,18 +4,30 @@ import { AuthContext } from './context/AuthContext';
 import { useRoutes } from './routes';
 import { useAuth } from './hooks/auth.hook';
 import { Navbar } from './components/Navbar';
+import { Loader } from './components/Loader';
 import 'materialize-css';
 
 function App() {
-  const { token, login, logout, userId } = useAuth();
-  const isAuthenticated = !!token
+  const { token, login, logout, userId, ready } = useAuth();
+  const isAuthenticated = !!token;
   const routes = useRoutes(isAuthenticated);
+
+  if (!ready) {
+    return <Loader />;
+  }
+
   return (
-    <AuthContext.Provider value={{
-      token, login, logout, userId, isAuthenticated
-    }}>
+    <AuthContext.Provider
+      value={{
+        token,
+        login,
+        logout,
+        userId,
+        isAuthenticated,
+      }}
+    >
       <Router>
-        { isAuthenticated && <Navbar />}
+        {isAuthenticated && <Navbar />}
         <div className="container">{routes}</div>
       </Router>
     </AuthContext.Provider>
