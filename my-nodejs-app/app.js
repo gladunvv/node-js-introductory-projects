38af05c7f58d1e1c64534ca864d9ba1e2ddf0822
vladislav <bincha.1997@gmail.com>
2020-03-08 22:18:20 +0900

    feat: initial project

---
+++ my-nodejs-app/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = 5000
+
+async function start() {
+    try {
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
+    } catch (e) {
+        console.log(`Server Error: ${e.message}`)
+        process.exit(1)
+    }
+}
+
+start()
