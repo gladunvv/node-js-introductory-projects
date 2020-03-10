55b434be9b4ceed584868a047a0738f8244179b6
vladislav <bincha.1997@gmail.com>
2020-03-10 23:17:21 +0900

    feat: added detail view

--- my-nodejs-app/routes/todo.routes.js
+++ my-nodejs-app/routes/todo.routes.js
@@ -5,11 +5,20 @@ const router = Router();
 router.get('/list', async (req, res) => {
   try {
     const todos = await Todo.find({});
-    if (!!todos) {
+    if (!todos) {
       res.json({ message: 'Записей нет' });
     } else {
       res.json(todos);
     }
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова...' });
+  }
+});
+
+router.get('/detail/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    res.json(todo);
   } catch (e) {
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова...' });
   }
